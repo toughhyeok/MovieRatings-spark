37a2b549b366101e23cec7207d3555b5bcfed859
toughhyeok <toughhyeok96@gmail.com>
2023-11-02 00:08:30 +0900

    min temperature example

---
+++ min-temperatures.py
@@ -0,0 +1,24 @@
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("MinTemperatures")
+sc = SparkContext(conf=conf)
+sc.setLogLevel("error")
+
+
+def parse_line(line):
+    fields = line.split(',')
+    station_id = fields[0]
+    entry_type = fields[2]
+    temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
+    return station_id, entry_type, temperature
+
+
+lines = sc.textFile("file:///Users/hotamul/SparkProjects/MovieRating/1800.csv")
+parsed_lines = lines.map(parse_line)
+min_temps = parsed_lines.filter(lambda x: "TMIN" in x[1])
+station_temps = min_temps.map(lambda x: (x[0], x[2]))
+min_temps = station_temps.reduceByKey(lambda x, y: min(x, y))
+results = min_temps.collect();
+
+for result in results:
+    print(result[0] + "\t{:.2f}F".format(result[1]))
